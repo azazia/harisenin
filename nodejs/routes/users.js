4fb7347767d46a495c7f8029bc598be14296a999
azazia <azaziahmad9@gmail.com>
2023-10-18 13:21:40 +0800

    rev

--- nodejs/routes/users.js
+++ nodejs/routes/users.js
@@ -3,7 +3,7 @@ import {v4 as uuidv4} from "uuid"
 
 const router = express.Router()
 
-const users = []
+let users = []
 
 //semua route disini akan berawalan /users
 // menampilkan semua user
@@ -18,6 +18,36 @@ router.post('/', (req,res) =>{
     users.push({...user, id: uuidv4()})
 
     res.send(`user ${user.firstName} telah ditambahkan ke database!`)
+})
+
+//find by id
+router.get('/:id', (req,res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id)
+
+    res.send(foundUser)
+})
+
+router.delete('/:id', (req,res) => {
+    const { id } = req.params
+
+    users = users.filter((user) => user.id !== id)
+
+    res.send(`user dengan id ${id} telah dihapus!`)
+})
+
+router.patch('/:id', (req,res) => {
+    const { id } = req.params
+    const { firstName, lastName, age} = req.body
+
+    const user = users.find((user) => user.id === id)
+
+    if(firstName) user.firstName = firstName
+    if(lastName) user.lastName = lastName
+    if(age) user.age = age
+
+    res.send(`user dengan id ${id} telah diupdate!`)
 })
 
 export default router;
