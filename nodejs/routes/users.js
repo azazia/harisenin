7fd38a1f0a90decf5249c74278a73288084b6d7e
azazia <azaziahmad9@gmail.com>
2023-10-22 23:42:37 +0800

    add controller

--- nodejs/routes/users.js
+++ nodejs/routes/users.js
@@ -1,53 +1,21 @@
 import express from "express";
-import {v4 as uuidv4} from "uuid"
+import { getUsers, createUser, getUser, deleteUser, updateUser } from "../controllers/users.js"
+
 
 const router = express.Router()
 
-let users = []
-
 //semua route disini akan berawalan /users
 // menampilkan semua user
-router.get('/', (req,res) =>{
-    res.send(users)
-})
+router.get('/', getUsers)
 
 // menambahkan user
-router.post('/', (req,res) =>{
-    const user = req.body
-
-    users.push({...user, id: uuidv4()})
-
-    res.send(`user ${user.firstName} telah ditambahkan ke database!`)
-})
+router.post('/', createUser)
 
 //find by id
-router.get('/:id', (req,res) => {
-    const { id } = req.params;
+router.get('/:id', getUser)
 
-    const foundUser = users.find((user) => user.id === id)
+router.delete('/:id', deleteUser)
 
-    res.send(foundUser)
-})
-
-router.delete('/:id', (req,res) => {
-    const { id } = req.params
-
-    users = users.filter((user) => user.id !== id)
-
-    res.send(`user dengan id ${id} telah dihapus!`)
-})
-
-router.patch('/:id', (req,res) => {
-    const { id } = req.params
-    const { firstName, lastName, age} = req.body
-
-    const user = users.find((user) => user.id === id)
-
-    if(firstName) user.firstName = firstName
-    if(lastName) user.lastName = lastName
-    if(age) user.age = age
-
-    res.send(`user dengan id ${id} telah diupdate!`)
-})
+router.patch('/:id', updateUser)
 
 export default router;
